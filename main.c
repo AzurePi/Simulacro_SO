663dc2a700bad85287e3affd7eb4eef7f4604e27
Pedro Cardana <90662531+AzurePi@users.noreply.github.com>
2024-05-06 17:49:43 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,11 @@
 #include <pthread.h>
 
 int main() {
+    pthread_attr_t atrib;
+    pthread_t t1, t10, t11, t6, t7, t2, t3;
+
+    pthread_attr_init(atrib);
+    pthread_attr_setscope(&atrib, PTHREAD_SCOPE_SYSTEM);
 
 
     return 0;
