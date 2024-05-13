f6369af73cbfb2173e01fe2fd01e886ac6bbbcbe
Pedro Cardana <90662531+AzurePi@users.noreply.github.com>
2024-05-12 21:06:18 -0300

    começo da leitura das informações dos comandos de um programa sintético (precisa ser finalizado)

--- sintetico.c
+++ sintetico.c
@@ -21,7 +21,7 @@ BCP *BCP_From_Sintetico(FILE *programa) {
 
     processo->estado = 0; //diz que o programa está pronto para ser executado
 
-    char *semaforos = (char *) malloc(51 * sizeof(char));
+    char semaforos[51];
     char caractere;
     int i = 0;
 
@@ -34,8 +34,47 @@ BCP *BCP_From_Sintetico(FILE *programa) {
 
     //para cada semáforo guardado, cria uma struct semáforo e coloca no BCP
 
+    //lê a linha em branco
+    fscanf(programa, "%*[^\n]s");
 
-    free(semaforos);
+    //lê cada um dos comandos do processo; guarda ele em uma lista, em que cada elemento tem um código de operação e um parâmetro
+    processo->comandos = malloc(sizeof(Lista_Comandos));
+    Definir(processo->comandos);
+
+    char buffer_comando[6], buffer_parametro[6];
+
+    while (fscanf(programa, "%s %s", buffer_comando, buffer_parametro)) {
+        //TODO: criar um novo Comando com base nos bufferes
+            //comparar a string lida com os possíveis comandos esperados, e com base nisso determinar o opcode
+        //TODO: adicionar o comando na Lista_Comandos usando Inserir()
+    }
+
     return processo;
 }
 
+bool Vazia(Lista_Comandos *l) {
+    return (l->nElementos == 0);
+}
+
+bool Cheia(Lista_Comandos *l) {
+    return (l->nElementos == MAX);
+}
+
+void Definir(Lista_Comandos *l) {
+    l->nElementos = 0;
+    l->head = NULL;
+}
+
+bool Inserir(Comando *comando, Lista_Comandos *lista) {
+    // Lista cheia
+    if (Cheia(lista))
+        return false; //Inserção falhou
+
+    Comando *aux = lista->head;
+    while (aux->prox != NULL) {
+        aux = aux->prox;
+    }
+    aux->prox = comando;
+    lista->nElementos++;
+    return true; //Inserção feita com sucesso
+}
--- sintetico.h
+++ sintetico.h
@@ -1,10 +1,24 @@
 #ifndef SIMULACRO_SO_SINTETICO_H
 #define SIMULACRO_SO_SINTETICO_H
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
 #include <string.h>
+
+#define MAX 100
+
+typedef struct comando {
+    int opcode;
+    int parametro;
+    struct comando *prox;
+} Comando;
+
+typedef struct {
+    Comando *head;
+    int nElementos;
+} Lista_Comandos;
 
 typedef struct {
     char nome[61]; //nome do programa
@@ -13,8 +27,25 @@ typedef struct {
     int tamanho_seg; //tamanho do segmento (em kbytes)
     int estado; //0 - pronto; 1 - executando; 2 - parado; 3 - terminado
     char *semaforos; //Lista de semáforos usados pelo programa
+    Lista_Comandos *comandos;
 } BCP;
 
 BCP *BCP_From_Sintetico(FILE *programa);
+
+//Retorna true (1) se vazia, false (0) caso contrário.
+bool Vazia(Lista_Comandos *l);
+
+//Retorna true (1) se cheia, false (0) caso contrário.
+bool Cheia(Lista_Comandos *l);
+
+//Inicializa uma Lista_Comandos vazia.
+void Definir(Lista_Comandos *l);
+
+/*
+Insere um novo comando no final de uma Lista_Comandos.
+Devolve true se sucesso, false caso contrário.
+*/
+bool Inserir(Comando *comando, Lista_Comandos *lista)
+
 
 #endif //SIMULACRO_SO_SINTETICO_H
