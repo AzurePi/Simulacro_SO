b7fcb67bdad9b28d897753fb9a8fdbebdc510b35
Pedro Cardana <90662531+AzurePi@users.noreply.github.com>
2024-05-15 00:16:14 -0300

    progressso na leitura dos programas sintéticos

--- sintetico.c
+++ sintetico.c
@@ -3,61 +3,84 @@
 BCP *BCP_From_Sintetico(FILE *programa) {
     //se não há programa para ler, retorna NULL
     if (!programa) {
-        printf("ERRO: arquivo não encontrado");
+        printf("ERRO: arquivo do programa sintético não encontrado");
         return NULL;
     }
 
     BCP *processo = malloc(sizeof(BCP));
+    if (!processo) {
+        printf("ERRO: falha na alocação de memória do BCP");
+        return NULL;
+    }
 
     // lê os campos do BCP no programa sintético; se houver erro na leitura, informa que há problema no arquivo
     if (!fscanf(programa, "%60s", processo->nome) ||
         !fscanf(programa, "%d", &processo->id_seg) ||
         !fscanf(programa, "%d", &processo->prioridade_OG) ||
-        !fscanf(programa, "%d", &processo->tamanho_seg)
-            ) {
-        printf("ERRO: programa sintético contém erro no BCP");
+        !fscanf(programa, "%d", &processo->tamanho_seg)) {
+        printf("ERRO: programa sintético contém erro no cabeçalho");
         return NULL;
     }
 
     processo->estado = 0; //diz que o programa está pronto para ser executado
 
-    char semaforos[51];
-    char caractere;
+    char semaforos[11]; //não uma string, mas um vetor de caracteres
+    char s;
     int i = 0;
 
     //enquanto não chegamos no fim da linha e há caracteres para ler e não ultrapassamos o máximo de 10 semáforos
-    while ((caractere = fgetc(programa)) != '\n' && caractere != EOF && i < 10) {
-        if (caractere != ' ') {
-            semaforos[i] = caractere; //guardamos o caractere
+    while ((s = fgetc(programa)) != '\n' && s != EOF && i < 10) {
+        if (s != ' ') {
+            semaforos[i] = s; //guardamos o s
             i++;
         }
     }
     semaforos[i] = '\0'; //finaliza a string
 
-    //TODO: para cada semáforo guardado, cria uma struct semáforo e coloca no BCP
+    //TODO: para cada semáforo guardado, cria um novo semáforo, coloca na lista de semáforos geral, e coloca na lista do BCP
 
     //lê a linha em branco
     fscanf(programa, "%*[^\n]s");
 
-    //lê cada um dos comandos do processo; guarda ele em uma lista, em que cada elemento tem um código de operação e um parâmetro
+    //lê cada um dos comandos do processo; guarda ele em uma lista em que cada elemento tem um código de operação e um parâmetro
     processo->comandos = novaListaComandos();
+    if (!processo->comandos) {
+        printf("ERRO: falha na criação de lista de comandos para o programa");
+        return NULL;
+    }
 
-    //TODO: Heloísa faça daqui pra frente
-    char buffer_comando[6], buffer_parametro[6];
+    //TODO: Heloísa faça daqui pra frente por favor <3
+    char buffer[11];
 
-    //lê cada comando, e guarda as strings nos buffers
-    while (fscanf(programa, "%s %s", buffer_comando, buffer_parametro)) {
-        //comparar a string lida com os possíveis comandos esperados, e com base nisso determinar o opcode
-        //TODO: criar um novoComando com o opcode e parâmetro
-        //TODO: adicionar o comando na Lista_Comandos usando inserirComando()
+    //lê cada comando como uma string e armazena no buffer
+    while (fscanf(programa, "%s", buffer)) {
+        char buffer_comando[6], buffer_parametro[6];
+        OPCODE opcode;
+        int parametro;
+
+        //se estamos lidando com as operações de semáforo, a string terá of formato "operação(parâmetro)"
+        if (buffer[0] == 'P' || buffer[0] == 'V') {
+            sscanf(buffer, "%s(%s)", buffer_comando, buffer_parametro);
+
+            //TODO: comparar buffer_comando com as strings esperadas para determinar a operação sendo realizada (nesse caso, só P ou V)
+            //TODO: converter o parâmetro (um caractere) em um valor inteiro para ser o parâmetro (como o char já é um número, não deve ter nenhum problema)
+
+        } else { //se estamos lidando com as demais operações, a string tera formato "operação parâmetro"
+            sscanf(buffer, "%s %s", buffer_comando, buffer_parametro);
+
+            //TODO: comparar buffer_comando com as strings esperadas para determinar a operação sendo realizada (todo o resto)
+            //TODO: transformar a string do parâmetro em um valor inteiro
+            //TODO: usar novoComando para criar um comando com o opcode e o parâmetro, e usar inserirComando para guardar isso no processo
+        }
+        //TODO: usar novoComando para criar um comando com o opcode e o parâmetro, e usar inserirComando para guardar isso no processo
     }
 
     return processo;
 }
 
-Comando *novoComando(int opcode, int parametro) {
+Comando *novoComando(OPCODE opcode, int parametro) {
     Comando *c = malloc(sizeof(Comando));
-    if (!c) return NULL;
+    if (!c) return NULL; // se a alocação de memória falhou
     c->opcode = opcode;
     c->parametro = parametro;
     c->prox = NULL;
@@ -70,16 +93,29 @@ bool vaziaListaComandos(Lista_Comandos *l) {
 
 Lista_Comandos *novaListaComandos() {
     Lista_Comandos *l = malloc(sizeof(Lista_Comandos));
-    if (!l) return NULL;
+    if (!l) return NULL; // se a alocação de memória falhou
     l->nElementos = 0;
     l->head = NULL;
     return l;
 }
 
 void inserirComando(Comando *comando, Lista_Comandos *lista) {
+    if (!lista || !comando) return; // se os parâmetros são ponteiros nulos
+
     Comando *aux = lista->head;
     while (aux->prox != NULL)
         aux = aux->prox;
     aux->prox = comando;
     lista->nElementos++;
+}
+
+void proc_sleep(BCP *proc) {
+    if (!proc) return; // se o processo passado não existe
+
+}
+
+void proc_wakeup(BCP *proc) {
+    if (proc) {
+        proc->estado = PRONTO;
+    }
 }
--- sintetico.h
+++ sintetico.h
@@ -6,44 +6,64 @@
 #include <stdlib.h>
 #include <malloc.h>
 #include <string.h>
+#include "semaforo.h"
 
+// Estado do processo
+typedef enum {
+    PRONTO, EXECUTANDO, PARADO, TERMINADO
+} ESTADO;
+
+// Operação a ser executada
+typedef enum {
+    EXEC, READ, WRITE, P, V, PRINT
+} OPCODE;
+
+// Estrutura de um comando, com seu opcode e parâmetro
 typedef struct comando {
-    int opcode; // 0 - exec; 1 - read; 2 - write; 3 - P(); 4 - V(); 5 - print
-    int parametro; //parâmetro do comando; tem um tratamento diferente, dependendo do opcode
+    OPCODE opcode; // EXEC, READ, WRITE, P, V ou PRINT
+    int parametro; // parâmetro do comando; tem um tratamento diferente, dependendo do opcode
     struct comando *prox;
 } Comando;
 
+// Lista de comandos presentes num dado programa
 typedef struct {
-    Comando *head;
-    int nElementos;
+    Comando *head; // começo da lista
+    int nElementos; // número de elementos da lista
 } Lista_Comandos;
 
-typedef struct {
-    char nome[61]; //nome do programa
-    int id_seg; //identificador do segmento
-    int prioridade_OG; //prioridade original do programa
-    int tamanho_seg; //tamanho do segmento (em kbytes)
-    int estado; //0 - pronto; 1 - executando; 2 - parado; 3 - terminado
-    char *semaforos; //Lista de semáforos usados pelo programa TODO: trocar o tipo para a struct de semáforo
-    Lista_Comandos *comandos; //Lista de comandos do programa
+typedef struct bcp {
+    char nome[61]; // nome do programa
+    int id_seg; // identificador do segmento
+    int prioridade_OG; // prioridade original do programa
+    int tamanho_seg; // tamanho do segmento (em kbytes)
+    ESTADO estado; // PRONTO, EXECUTANDO, PARADO ou TERMINADO
+    semaphore_t *semaforos; // Lista de semáforos usados pelo programa
+    Lista_Comandos *comandos; // Lista de comandos do programa
+    struct bcp *prox; // ponteiro para o próximo processo na lista
 } BCP;
 
-//Cria um BCP com base na leitura de um programa sintético
+// Cria um BCP com base na leitura de um programa sintético
 BCP *BCP_From_Sintetico(FILE *programa);
 
-//Inicializa um novo Comando com base nos parâmetros
-Comando *novoComando(int opcode, int parametro);
+// Inicializa um novo Comando com base nos parâmetros
+Comando *novoComando(OPCODE opcode, int parametro);
 
-//Retorna true (1) se vazia, false (0) caso contrário.
+// Retorna true (1) se vazia, false (0) caso contrário.
 bool vaziaListaComandos(Lista_Comandos *l);
 
-//Inicializa uma Lista_Comandos vazia.
-Lista_Comandos * novaListaComandos();
+// Inicializa uma Lista_Comandos vazia.
+Lista_Comandos *novaListaComandos();
 
 /*
-Insere um novo comando no final de uma Lista_Comandos.
-Devolve true se sucesso, false caso contrário.
+    Insere um novo comando no final de uma Lista_Comandos.
+    Devolve true se sucesso, false caso contrário.
 */
 void inserirComando(Comando *comando, Lista_Comandos *lista);
+
+// muda o estado do processo para BLOQUEADO e o manda pro final da fila de escalonamento
+void proc_sleep(BCP *proc);
+
+// muda o estado do processo para PRONTO
+void proc_wakeup(BCP *proc);
 
 #endif //SIMULACRO_SO_SINTETICO_H
