439c476e423207c13efc4b382ee30ed7cb15daa6
Suzu <lucas-xavier.pereira@unesp.br>
2024-05-14 23:19:23 -0300

    botei o semaforo

--- eventos.c
+++ eventos.c
@@ -2,9 +2,32 @@
 
 void processInterrupt() {}
 
-void semaphoreP(int v) {}
+void semaphoreP(semaphore_t* semaph, BCPitem_t* proc){
+    pthread_mutex_lock(&semaph->mutex_lock);
+    if(semaph->v < 0)
+    {
+        sem_queue(&semaph->waiting_list,proc);
+        proc_sleep(proc);
+    }
+    semaph->v--;
+    pthread_mutex_unlock(&semaph->mutex_lock);
+}
 
-void semaphoreV(int v) {}
+void semaphoreV(semaphore_t* semaph)
+{
+    pthread_mutex_lock(&semaph->mutex_lock);
+    semaph->v++;
+    if(semaph->v <= 0)
+    {
+        if(semaph->waiting_list)
+        {
+            BCPitem_t* proc = semaph->waiting_list->proc;
+            semaph->waiting_list = semaph->waiting_list->next;
+            proc_wakeup(proc);
+        }
+    }
+    pthread_mutex_unlock(&semaph->mutex_lock);
+}
 
 void DiskRequest() {}
 
--- eventos.h
+++ eventos.h
@@ -1,7 +1,30 @@
 #ifndef SIMULACRO_SO_EVENTOS_H
 #define SIMULACRO_SO_EVENTOS_H
+#include <pthread.h>
 
 int relogio = 0;
+
+//struct semaforo
+typedef struct semaphore {
+    pthread_mutex_t mutex_lock;
+    volatile int v; //valor do semaforo
+    char name;
+    int refcount; //conta o numero de processos que esta usando o semaforo
+    struct sem_li* waiting_list; // topo da lista de espera de processos do semaforo
+    struct semaphor* ne;
+} semaphore_t;
+
+typedef struct sem_li //lista de processos esperando o semaforo
+{
+    BCPitem_t* proc;
+    struct sem_li* next;
+} sem_list_item_t;
+
+typedef struct all_sem_li //lista de todos semaforos
+{
+    semaphore_t* head;
+} all_sem_list_t;
+
 
 // interrupção gerada pela execução da função processCreate e pelo término de operações de E/S
 void processInterrupt();
