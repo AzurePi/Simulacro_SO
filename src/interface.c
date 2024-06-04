a38d2f0d53c5b5a7cc0b9d82e87e0bae51e7822f
Pedro Cardana <90662531+AzurePi@users.noreply.github.com>
2024-06-04 00:20:54 -0300

    arrumaçõezinhos

--- src/interface.c
+++ src/interface.c
@@ -1,38 +1,11 @@
 #include "include/interface.h"
 
-void draw_borders(WINDOW* win)
-{
+void draw_borders(WINDOW *win) {
     box(win, 0, 0);
     wrefresh(win);
-
-    /*
-    int x, y, i;
-    getmaxyx(win, y, x);
-
-    // Desenha bordas horizontais
-    for (i = 0; i <= x; i++)
-    {
-        mvwprintw(win, 0, i, "-");
-        mvwprintw(win, y - 1, i, "-");
-    }
-
-    // Desenha bordas verticais
-    for (i = 0; i <= y; i++)
-    {
-        mvwprintw(win, i, 0, "|");
-        mvwprintw(win, i, x - 1, "|");
-    }
-
-    // Desenha os cantos
-    mvwprintw(win, 0, 0, "+");
-    mvwprintw(win, 0, x - 1, "+");
-    mvwprintw(win, y - 1, 0, "+");
-    mvwprintw(win, y - 1, x - 1, "+");
-    */
 }
 
-void initializeInterface()
-{
+void initializeInterface() {
     initscr();
     start_color();
     cbreak();
@@ -49,22 +22,22 @@ void initializeInterface()
     // Definindo dimensões e posições das janelas ----------------------------------------------------------------------
 
     // Menu na parte superior
-    WINDOW* win_super_menu = newwin(menu_lines, middle, 0, 0);
+    WINDOW *win_super_menu = newwin(menu_lines, middle, 0, 0);
 
     // Processo em Execução ao lado do Menu
-    WINDOW* win_super_exec_proc = newwin(menu_lines, middle, 0, middle);
+    WINDOW *win_super_exec_proc = newwin(menu_lines, middle, 0, middle);
 
     // Log de Erros na parte inferior
-    WINDOW* win_super_error_log = newwin(error_lines, width, menu_lines, 0);
+    WINDOW *win_super_error_log = newwin(error_lines, width, menu_lines, 0);
 
     // Lista de Processos abaixo do Estado da Memória (lado esquerdo)
-    WINDOW* win_super_processos = newwin(mid_lines, middle, menu_lines + error_lines, 0);
+    WINDOW *win_super_processos = newwin(mid_lines, middle, menu_lines + error_lines, 0);
 
     // Lista de Semáforos abaixo do Estado da Memória (lado direito)
-    WINDOW* win_super_semaforos = newwin(mid_lines, middle, menu_lines + error_lines, middle);
+    WINDOW *win_super_semaforos = newwin(mid_lines, middle, menu_lines + error_lines, middle);
 
     // Estado da Memória abaixo do Menu
-    WINDOW* win_super_mem_state = newwin(mem_lines, width, menu_lines + error_lines + mid_lines, 0);
+    WINDOW *win_super_mem_state = newwin(mem_lines, width, menu_lines + error_lines + mid_lines, 0);
 
 
     // Definindo a parte interna das janelas, que é onde escreveremos de fato
@@ -75,13 +48,10 @@ void initializeInterface()
     win_semaforos = derwin(win_super_semaforos, mid_lines - 2, middle - 2, 1, middle + 1);
     win_mem_state = derwin(win_super_mem_state, mem_lines - 2, width - 2, 1, 1);
 
-    /*
     // Habilitar scrolling em algumas janelas
     scrollok(win_processos, TRUE);
     scrollok(win_semaforos, TRUE);
     scrollok(win_error_log, TRUE);
-    */
-
 
     // Desenha as bordas das janelas
     draw_borders(win_super_menu);
@@ -90,7 +60,6 @@ void initializeInterface()
     draw_borders(win_super_processos);
     draw_borders(win_super_semaforos);
     draw_borders(win_super_error_log);
-
 
     // Atualiza as janelas com as bordas desenhadas
     wrefresh(win_super_menu);
@@ -101,10 +70,8 @@ void initializeInterface()
     wrefresh(win_super_error_log);
 }
 
-void* menu()
-{
-    while (!encerrar)
-    {
+void *menu() {
+    while (!encerrar) {
         wclear(win_menu); // limpa a janela
 
         mvwprintw(win_menu, 0, 1, " [1] Novo processo");
@@ -116,15 +83,13 @@ void* menu()
         char op = wgetch(win_menu); // Captura a entrada do usuário
         flushinp();
 
-        switch (op)
-        {
-        case '0':
-            mvwprintw(win_menu, 4, 1, "Encerrando programa...");
-            wrefresh(win_menu);
-            encerrar = true;
-            break;
-        case '1':
-            {
+        switch (op) {
+            case '0':
+                mvwprintw(win_menu, 4, 1, "Encerrando programa...");
+                wrefresh(win_menu);
+                encerrar = true;
+                break;
+            case '1': {
                 char filename[201];
 
                 mvwprintw(win_menu, 4, 1, "Caminho do programa: ");
@@ -133,15 +98,15 @@ void* menu()
                 flushinp();
 
                 // passamos uma cópia, para evitar que filename se perca no fim do bloco do case
-                char* filename_copy = strdup(filename);
+                char *filename_copy = strdup(filename);
                 sysCall(process_create, filename_copy);
             }
-            break;
-        default:
-            mvwprintw(win_menu, 4, 1, "Opção inválida!"); //TODO: formatação de um erro
-            wrefresh(win_menu);
-            napms(1000);
-            break;
+                break;
+            default:
+                mvwprintw(win_menu, 4, 1, "Opção inválida!"); //TODO: formatação de um erro
+                wrefresh(win_menu);
+                napms(1000);
+                break;
         }
         napms(1000);
     }
@@ -149,56 +114,50 @@ void* menu()
     return NULL;
 }
 
-void* processandoAgora()
-{
-    while (!encerrar)
-    {
-        if (!refresh_atual)
+void *processandoAgora() {
+    while (!encerrar) {
+        if (!refresh_atual) {
+            nsleep(500);
             continue;
+        }
+        refresh_atual = false;
 
         wclear(win_exec_proc);
         mvwprintw(win_exec_proc, 0, 0, "Processando agora: ");
 
         pthread_mutex_lock(&mutex_lista_processos); // bloqueia acesso à lista de processos
-        if (executando_agora)
-        {
+        if (executando_agora) {
             char nome[61];
             strcpy(nome, executando_agora->nome);
             mvwprintw(win_exec_proc, 0, 20, "%s (%d)", nome, executando_agora->id_seg);
-        }
-        else
+        } else
             mvwprintw(win_exec_proc, 0, 20, "nada");
         pthread_mutex_unlock(&mutex_lista_processos); // libera acesso à lista de processos
+
         wrefresh(win_exec_proc);
-        refresh_atual = false;
     }
     return NULL;
 }
 
-void* informacaoProcessos()
-{
-    while (!encerrar)
-    {
-        if (!refresh_proc)
+void *informacaoProcessos() {
+    while (!encerrar) {
+        if (!refresh_proc) {
+            nsleep(500);
             continue;
+        }
+        refresh_proc = false;
 
         wclear(win_processos);
 
         pthread_mutex_lock(&mutex_lista_processos); // bloqueia acesso à lista de processos
-        BCP* aux = head_lista_processos;
+        BCP *aux = head_lista_processos;
         if (!aux)
-        {
             mvwprintw(win_processos, 0, 1, "Ainda não há processos no sistema.");
-            pthread_mutex_unlock(&mutex_lista_processos); // libera acesso à lista de processos
-        }
-        else
-        {
-            char* estados[] = {"PRONTO", "EXECUTANDO", "BLOQUEADO", "TERMINADO"};
-
+        else {
+            char *estados[] = {"PRONTO", "EXECUTANDO", "BLOQUEADO", "TERMINADO"};
             int i = 0;
 
-            while (aux)
-            {
+            while (aux) {
                 // monta uma linha com as informações do processo
                 char linha[128];
                 snprintf(linha, sizeof(linha),
@@ -209,91 +168,84 @@ void* informacaoProcessos()
 
                 aux = aux->prox;
             }
-            pthread_mutex_unlock(&mutex_lista_processos); // libera acesso à lista de processos
         }
+        pthread_mutex_unlock(&mutex_lista_processos); // libera acesso à lista de processos
 
         wrefresh(win_processos);
-        refresh_proc = false;
     }
     return NULL;
 }
 
-void* informacaoSemaforos()
-{
-    while (!encerrar)
-    {
-        if (!refresh_sem)
+void *informacaoSemaforos() {
+    while (!encerrar) {
+        if (!refresh_sem) {
+            nsleep(500);
             continue;
+        }
+        refresh_sem = false;
 
         wclear(win_semaforos);
+        mvwprintw(win_semaforos, 0, 1, "Lista de Semáforos sendo utilizados: ");
 
-        mvwprintw(win_semaforos, 0, 0, "Lista de Semáforos sendo utilizados: ");
         pthread_mutex_lock(&mutex_semaforos_globais); // bloqueia acesso à lista de semáforos
-        No_Semaforo* aux = semaforos_existentes->head;
+        No_Semaforo *aux = semaforos_existentes->head;
         if (!aux)
-        {
-            mvwprintw(win_semaforos, 1, 5, "Nenhum");
-            pthread_mutex_unlock(&mutex_semaforos_globais); // libera acesso à lista de semáforos
-        }
-        else
-        {
+            mvwprintw(win_semaforos, 1, 6, "Nenhum");
+        else {
             int i = 1;
 
-            while (aux)
-            {
+            while (aux) {
                 // monta a linha com as informações do semáforo
                 char linha[128];
                 snprintf(linha, sizeof(linha),
                          "%c | Contador: %d processos",
                          aux->semaforo->nome, aux->semaforo->refcount);
 
-                mvwprintw(win_semaforos, i++, 5, "%s", linha);
+                mvwprintw(win_semaforos, i++, 6, "%s", linha);
 
                 aux = aux->prox;
             }
-            pthread_mutex_unlock(&mutex_semaforos_globais); // libera acesso à lista de semáforos
         }
+        pthread_mutex_unlock(&mutex_semaforos_globais); // libera acesso à lista de semáforos
+
         wrefresh(win_semaforos);
-        refresh_sem = false;
     }
     return NULL;
 }
 
-void* informacaoMemoria()
-{
-    while (!encerrar)
-    {
-        if (!refresh_mem)
+void *informacaoMemoria() {
+    while (!encerrar) {
+        if (!refresh_mem) {
+            nsleep(500);
             continue;
+        }
+        refresh_mem = false;
 
         wclear(win_mem_state);
 
         pthread_mutex_lock(&mutex_RAM); // bloqueia acesso à RAM
 
         const int paginas_ocupadas = RAM->n_paginas_ocupadas;
-        const double taxa_ocupacao = (double)paginas_ocupadas / NUMERO_PAGINAS * 100.0;
+        const double taxa_ocupacao = (double) paginas_ocupadas / NUMERO_PAGINAS * 100.0;
 
-        mvwprintw(win_mem_state, 0, 0, "Taxa de ocupacao da memoria: %.2f%% (%d/%d)", taxa_ocupacao,
+        mvwprintw(win_mem_state, 0, 1, "Taxa de ocupacao da memoria: %.2f%% (%d/%d)", taxa_ocupacao,
                   paginas_ocupadas, NUMERO_PAGINAS);
 
         pthread_mutex_unlock(&mutex_RAM); // libera acesso à RAM
 
         wrefresh(win_mem_state);
-        refresh_mem = false;
     }
     return NULL;
 }
 
-BCP* mensagemErroBCP(const char* mensagem, BCP* processo)
-{
+BCP *mensagemErroBCP(const char *mensagem, BCP *processo) {
     if (!mensagem || !processo) return NULL;
+    static i = 0;
 
     wclear(win_error_log);
-    mvwprintw(win_error_log, 0, 0, "%s", mensagem);
+    mvwprintw(win_error_log, i, 0, "%s", mensagem);
     wrefresh(win_error_log);
-
-    //napms(2000);
-
+    i++;
     freeBCP(processo);
     return NULL;
 }
